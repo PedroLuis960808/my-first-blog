c6944fb80e85e8b1657f3ec77572b1b7d5cd2000
saulhp <pluiis83@gmail.com>
2019-11-11 12:43:08 -0600

    Templates modificados para mostrar post desde base de datos2.

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .models import Post
 
 def post_list(request):
     post=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'post':post_list})
+    return render(request, 'blog/post_list.html', {'post':post})
 
 
 
