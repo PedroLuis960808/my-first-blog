8fb3e615d1e00ae798443a9693dc762e9812db1c
saulhp <pluiis83@gmail.com>
2019-11-10 18:50:13 -0600

    Cambie el HTML para la página.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {}) 
+
+##esto es una funcion def y regresa una funcion render que construye nuestra plantilla.
